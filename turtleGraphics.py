1dcbc64aa7aec2cd4a12914ca3257fcbea3b8f1f
d00741845 <service.awg@gmail.com>
2019-01-22 21:54:26 +0000

    Initial commit

---
+++ turtleGraphics.py
@@ -0,0 +1,23 @@
+"""
+Python Turtle Module
+https://youtu.be/p7CiFhiTdvY
+"""
+
+import turtle
+
+tim = turtle.Turtle()
+tim.color('red')
+tim.pensize(5)
+tim.shape('turtle')
+
+tim.forward(100)
+tim.left(90)
+tim.penup()
+tim.forward(100)
+tim.right(90)
+tim.pendown()
+tim.color('green')
+tim.forward(100)
+tim.speed()
+
+
